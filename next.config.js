b10f3d31b0d60cebd1885ee38b9ad2a3fa5fbbd4
Ali-Ahmed999 <150151136+Ali-Ahmed999@users.noreply.github.com>
2025-01-14 14:30:51 +0500

    next.config.js eslint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     // Add any experimental features you want to enable here
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
